ea4c4a6b5dbb568d2ccf28f5bd9d312293ee8cd1
Cade Ward <cadebward@gmail.com>
2018-11-06 09:45:24 -0700

    add simple web component

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,34 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+// Usage:
+// <app-drawer trigger="#trigger" content="#content">
+//   <button id="trigger">Click Here</button>
+//   <div id="content">
+//     Here is some hidden content
+//  </div>
+// </app-drawer>
+class AppDrawer extends HTMLElement {
+  constructor() {
+    super()
+    const button = this.querySelector(this.getAttribute("trigger"))
+    const el = this.querySelector(this.getAttribute("content"))
+    button.addEventListener("click", e => this.toggle(el))
+    this.toggle(el)
+  }
+
+  get open() {
+    return this.hasAttribute("open")
+  }
+
+  toggle(el) {
+    if (el.style.display == "") {
+      el.style.display = "none"
+    } else {
+      el.style.display = ""
+    }
+  }
+}
+
+window.customElements.define('app-drawer', AppDrawer)
